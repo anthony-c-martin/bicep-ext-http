28c769309c3284e0c201ac6b1e0d035413c866ba
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2026-07-11 01:59:38 -0400

    Use Bicep v0.45.6

--- samples/basic/main.bicep
+++ samples/basic/main.bicep
@@ -1,3 +1,5 @@
+targetScope = 'local'
+
 extension http
 
 param coords {
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,15 +8,23 @@ root="$(dirname ${BASH_SOURCE[0]})/../src"
 types_index="$(dirname ${BASH_SOURCE[0]})/../types/index.json"
 ext_name="bicep-ext-http"
 
+# prefer bicep from $PATH, fall back to ~/.azure/bin/bicep
+bicep_cmd=$(command -v bicep || echo "$HOME/.azure/bin/bicep")
+
+# build various flavors
 env GOOS=darwin GOARCH=arm64 go build -C $root -o "build/http-osx-arm64"
 env GOOS=linux GOARCH=amd64 go build -C $root -o "build/http-linux-x64"
+env GOOS=linux GOARCH=arm64 go build -C $root -o "build/http-linux-arm64"
 env GOOS=windows GOARCH=amd64 go build -C $root -o "build/http-win-x64.exe"
+env GOOS=windows GOARCH=arm64 go build -C $root -o "build/http-win-arm64.exe"
 
 # publish to the registry
-~/.azure/bin/bicep publish-extension \
+"$bicep_cmd" publish-extension \
   $types_index \
   --bin-osx-arm64 "$root/build/http-osx-arm64" \
   --bin-linux-x64 "$root/build/http-linux-x64" \
+  --bin-linux-arm64 "$root/build/http-linux-arm64" \
   --bin-win-x64 "$root/build/http-win-x64.exe" \
+  --bin-win-arm64 "$root/build/http-win-arm64.exe" \
   --target "$target" \
   --force
